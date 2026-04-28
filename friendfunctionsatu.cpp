bb35cabbd6e81c93d77ae8ba40a1b459cbc3459d
suhilmi01 <suhilmi01@mail.com>
2026-04-28 15:39:01 +0700

    mmebuat class mahasiswa

--- friendfunctionsatu.cpp
+++ friendfunctionsatu.cpp
@@ -2,3 +2,16 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+private:
+    string nama;
+public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
